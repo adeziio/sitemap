34a264d8bd1673915bb643e082bf31ec4e7dd749
Aden <adeziio@yahoo.com>
2022-04-03 15:17:47 -0400

    initial commit and setup

---
+++ src/components/layout/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './../css/style.css';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+
+    }
+
+    render() {
+
+        return (
+            <>
+                Home
+            </>
+        )
+    }
+}
