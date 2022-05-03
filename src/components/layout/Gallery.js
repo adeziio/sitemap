6a496e2add231d23606df4c7818be1be18f7ffdc
Aden <adeziio@yahoo.com>
2022-05-03 10:53:58 -0400

    add scroll up and pull to refresh

--- src/components/layout/Gallery.js
+++ src/components/layout/Gallery.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Alert, ImageList } from '@mui/material';
+import { KeyboardArrowUp } from '@mui/icons-material';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+import ScrollToTop from "react-scroll-to-top";
 import GalleryPhoto from "./GalleryPhoto";
 import { getGallery } from "./../api/BackendAPI";
 
@@ -38,25 +41,28 @@ export default class Gallery extends Component {
 
         return (
             <>
+                <ScrollToTop smooth color="#ffffff" style={{ backgroundColor: "#005b96" }} component={<KeyboardArrowUp sx={{ color: "#ffffff" }} />} />
                 {gallery.length !== 0 ?
-                    <ImageList
-                        variant="masonry"
-                        sx={{
-                            columnCount: {
-                                xs: '1 !important',
-                                sm: '2 !important',
-                                md: '3 !important',
-                                lg: '4 !important',
-                                xl: '5 !important',
-                            },
-                        }}
-                    >
-                        {gallery.map((item) => {
-                            return (
-                                <GalleryPhoto key={item.key} item={item} isAdmin={isAdmin} getGallery={this.getGallery} />
-                            )
-                        })}
-                    </ImageList>
+                    <PullToRefresh onRefresh={this.getGallery}>
+                        <ImageList
+                            variant="masonry"
+                            sx={{
+                                columnCount: {
+                                    xs: '1 !important',
+                                    sm: '2 !important',
+                                    md: '3 !important',
+                                    lg: '4 !important',
+                                    xl: '5 !important',
+                                },
+                            }}
+                        >
+                            {gallery.map((item) => {
+                                return (
+                                    <GalleryPhoto key={item.key} item={item} isAdmin={isAdmin} getGallery={this.getGallery} />
+                                )
+                            })}
+                        </ImageList>
+                    </PullToRefresh>
                     : resMsg === "Failed" ?
                         <Alert sx={{ marginTop: 1 }} severity="error">{`Failed to load gallery`}</Alert>
                         : null
