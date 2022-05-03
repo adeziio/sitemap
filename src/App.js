b91c83fb8e69b0f25f2bbb22f9e1a5585d1d1fd9
Aden <adeziio@yahoo.com>
2022-05-03 01:46:20 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -8,22 +8,37 @@ import {
 import Home from './components/layout/Home';
 import FocusedView from './components/layout/FocusedView';
 import { Helmet } from "react-helmet";
+import notFound from './../logo/not-found.jpg';
+import logoIcon from './../logo/logo-icon.png';
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <meta name="description" content="A photo collection created by Aden Tran." />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Star Gallery" />
-        <meta property="og:description" content="A photo collection created by Aden Tran." />
-        <meta property="og:image" content="%PUBLIC_URL%/favicon.ico" />
-        <meta property="og:url" content="https://stargallery.vercel.app/" />
-      </Helmet>
+
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/photo" element={<FocusedView />} />
+          <Route path="/" element={
+            <>
+              <Helmet>
+                <meta name="description" content="A photo collection created by Aden Tran." />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Star Gallery" />
+                <meta property="og:description" content="A photo collection created by Aden Tran." />
+                <meta property="og:image" content={logoIcon} />
+                <meta property="og:url" content="https://stargallery.vercel.app/" />
+              </Helmet>
+              <Home />
+            </>
+          } />
+          <Route path="/photo" element={
+            <>
+              <Helmet>
+                <meta property="og:description" content="" />
+                <meta property="og:image" content={notFound} />
+              </Helmet>
+              <FocusedView />
+            </>
+          } />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </Router>
