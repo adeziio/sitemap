fa30a49e35c95cf71d33a478bb3b4f663d03cc4b
Aden <adeziio@yahoo.com>
2022-05-04 00:11:46 -0400

    testing

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -13,20 +13,17 @@ export default class About extends Component {
 
         return (
             <>
-                <Grid container spacing={2} marginTop="1rem">
-
-                    <Grid item xs={16} >
-                        <Typography variant="h5" color="text.primary" fontSize="1.5rem" fontWeight="bold" display="block">
-                            Welcome to Star Gallery!
-                        </Typography>
-                        <Typography variant="p" color="text.primary" fontSize="1rem" display="block">
-                            Click <span style={{ fontWeight: "bold" }}>Upload</span> to submit your own photo
-                        </Typography>
-                        <Typography variant="p" color="text.primary" fontSize="1rem" display="block">
-                            Gallery Size: <span style={{ fontWeight: "bold" }}>{size}</span>
-                        </Typography>
-                    </Grid>
-                </Grid>
+                <div style={{ marginTop: "2rem" }}>
+                    <Typography variant="h5" color="text.primary" fontSize="1.5rem" fontWeight="bold" display="block">
+                        Welcome to Star Gallery!
+                    </Typography>
+                    <Typography variant="p" color="text.primary" fontSize="1rem" display="block">
+                        Click <span style={{ fontWeight: "bold" }}>Upload</span> to submit your own photo
+                    </Typography>
+                    <Typography variant="p" color="text.primary" fontSize="1rem" display="block">
+                        Gallery Size: <span style={{ fontWeight: "bold" }}>{size}</span>
+                    </Typography>
+                </div>
             </>
         )
     }
