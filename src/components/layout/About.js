1dce478bca3968c0bbeaec1bac9fab2f46122384
Aden <adeziio@yahoo.com>
2022-05-04 00:15:49 -0400

    add margin to texts

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -14,13 +14,13 @@ export default class About extends Component {
         return (
             <>
                 <div style={{ marginTop: "2rem" }}>
-                    <Typography variant="h5" color="text.primary" fontSize="1.5rem" fontWeight="bold" display="block">
+                    <Typography variant="h5" color="text.primary" fontSize="1.5rem" fontWeight="bold" margin="1rem" >
                         Welcome to Star Gallery!
                     </Typography>
-                    <Typography variant="p" color="text.primary" fontSize="1rem" display="block">
+                    <Typography variant="p" color="text.primary" fontSize="1rem" display="block" margin="1rem" >
                         Click <span style={{ fontWeight: "bold" }}>Upload</span> to submit your own photo
                     </Typography>
-                    <Typography variant="p" color="text.primary" fontSize="1rem" display="block">
+                    <Typography variant="p" color="text.primary" fontSize="1rem" display="block" margin="1rem" >
                         Gallery Size: <span style={{ fontWeight: "bold" }}>{size}</span>
                     </Typography>
                 </div>
