1a787cdb9d21a09794616ce107f926310759a7b1
Aden <adeziio@yahoo.com>
2022-05-09 01:33:29 -0400

    improve backend api call handling for errors

--- src/api/BackendAPI.js
+++ src/api/BackendAPI.js
@@ -1,14 +1,20 @@
 export const getGallery = async () => {
-    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/gallery`, {
-        "method": "GET",
-        "headers": {
-            'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
+    try {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/gallery`, {
+            "method": "GET",
+            "headers": {
+                'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
+            }
+        })
+        if (res.status === 200) {
+            return await res.json();
         }
-    }).catch(err => {
+        else {
+            return false;
+        }
+    }
+    catch (error) {
         return false;
-    })
-    if (res) {
-        return await res.json();
     }
 }
 
@@ -16,44 +22,62 @@ export const uploadFile = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/upload`, {
-        "method": "POST",
-        "headers": {
-            'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
-        },
-        "body": formData
-    }).catch(err => {
+    try {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/upload`, {
+            "method": "POST",
+            "headers": {
+                'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
+            },
+            "body": formData
+        })
+        if (res.status === 200) {
+            return await res.json();
+        }
+        else {
+            return false;
+        }
+    }
+    catch (error) {
         return false;
-    })
-    if (res) {
-        return await res.json();
     }
 }
 
 export const extractKey = async (key) => {
-    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/extract?key=${key}`, {
-        "method": "GET",
-        "headers": {
-            'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
+    try {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/extract?key=${key}`, {
+            "method": "GET",
+            "headers": {
+                'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
+            }
+        })
+        if (res.status === 200) {
+            return await res.json();
         }
-    }).catch(err => {
+        else {
+            return false;
+        }
+    }
+    catch (error) {
         return false;
-    })
-    if (res) {
-        return await res.json();
     }
 }
 
 export const deleteKey = async (key) => {
-    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/delete?key=${key}`, {
-        "method": "GET",
-        "headers": {
-            'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
+    try {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/delete?key=${key}`, {
+            "method": "GET",
+            "headers": {
+                'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
+            }
+        })
+        if (res.status === 200) {
+            return await res.json();
         }
-    }).catch(err => {
+        else {
+            return false;
+        }
+    }
+    catch (error) {
         return false;
-    })
-    if (res) {
-        return await res.json();
     }
 }
