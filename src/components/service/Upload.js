50061a87998e5daa8ec7ead28e080412954662d7
Aden <adeziio@yahoo.com>
2022-06-02 15:38:50 -0400

    upgrade upload page, add loading to submit

--- src/components/service/Upload.js
+++ src/components/service/Upload.js
@@ -1,105 +1,109 @@
-import React, { Component } from 'react';
-import { Button, FormControl, Alert, Card, CardMedia } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, FormControl, Alert, Card, CardMedia, CircularProgress, Dialog, DialogContent } from '@mui/material';
 import { FileUploader } from "react-drag-drop-files";
 import { uploadFile } from "../../api/BackendAPI";
 
-export default class Upload extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: undefined,
-            previewSrc: "",
-            resMsg: ""
-        }
-    }
+const Upload = (props) => {
+    const [file, setFile] = useState(undefined);
+    const [previewSrc, setPreviewSrc] = useState("");
+    const [isLoading, setIsloading] = useState(false);
+    const [dialog, setDialog] = useState(false);
+    const [resMsg, setResMsg] = useState("");
+    const fileTypes = ["jpeg", "jpg", "png", "gif"];
 
-    setResMsg = (resMsg) => {
-        this.setState({
-            resMsg: resMsg
-        })
-    }
-
-    handleFileSelect = (file) => {
-        if (file) {
+    const handleFileSelect = (fileUpload) => {
+        if (fileUpload) {
             let reader = new FileReader();
-            reader.readAsDataURL(file);
+            reader.readAsDataURL(fileUpload);
             reader.onloadend = (e) => {
-                this.setState({
-                    file: file,
-                    previewSrc: reader.result,
-                    resMsg: ""
-                })
+                setFile(fileUpload);
+                setPreviewSrc(reader.result);
+                setResMsg("");
             }
         }
         else {
-            this.setState({
-                file: file,
-                previewSrc: "",
-                resMsg: ""
-            })
+            setFile(file);
+            setPreviewSrc("");
+            setResMsg("");
         }
-    }
+    };
 
-    handleSubmit = async () => {
-        const { file } = this.state;
-
+    const handleSubmit = async () => {
         if (file) {
+            toggleDialog();
+            setIsloading(true);
             const result = await uploadFile(file);
             if (result) {
-                this.setResMsg(result.status);
+                setResMsg(result.status);
             }
             else {
-                this.setResMsg("Failed");
+                setResMsg("Failed");
             }
+            setIsloading(false);
+            toggleDialog();
         }
         else {
-            this.setResMsg("No file selected");
+            setResMsg("No file selected");
         }
-    }
+    };
 
-    render() {
-        const { resMsg, previewSrc } = this.state;
-        const fileTypes = ["jpeg", "jpg", "png", "gif"];
+    const toggleDialog = () => {
+        setDialog(prevDialog => !prevDialog);
+    };
 
-        const focusedView = (src) => {
-            const newTab = window.open();
-            newTab?.document.write(`<!DOCTYPE html><head><title></title></head><body><img src="${src}" width="100%" height="auto" ></body></html>`);
-            newTab?.document.close();
-        };
+    const handleDialogClick = e => {
+        e.stopPropagation();
+    };
 
-        return (
-            <>
-                <FormControl variant="standard" sx={{ width: "22rem", marginTop: "2rem" }}>
-                    <FileUploader handleChange={this.handleFileSelect} name="input" types={fileTypes} />
-                    <Button
-                        variant="contained"
-                        component="label"
-                        sx={{ marginTop: 1 }}
-                        onClick={this.handleSubmit}
-                    >
-                        Submit
-                    </Button>
-                </FormControl>
+    const focusedView = (src) => {
+        const newTab = window.open();
+        newTab?.document.write(`<!DOCTYPE html><head><title></title></head><body><img src="${src}" width="100%" height="auto" ></body></html>`);
+        newTab?.document.close();
+    };
 
-                {resMsg === "Success" ? <Alert sx={{ marginTop: 1 }} severity="success">{resMsg}</Alert> : null}
-                {resMsg === "No file selected" ? <Alert sx={{ marginTop: 1 }} severity="warning">{resMsg}</Alert> : null}
-                {resMsg === "Failed" ? <Alert sx={{ marginTop: 1 }} severity="error">{`Failed to upload`}</Alert> : null}
-
-                {previewSrc ?
-                    <Card
-                        sx={{ height: "100%", width: "100%", maxWidth: "20rem", display: "block", m: 2 }}
-                        elevation={5}
-                    >
-                        <CardMedia
-                            component="img"
-                            width="100%"
-                            image={previewSrc}
-                            alt={"img"}
-                            onClick={() => focusedView(previewSrc)}
-                        />
-                    </Card> : null
+    return (
+        <>
+            <FormControl variant="standard" sx={{ width: "22rem", marginTop: "2rem" }}>
+                <FileUploader handleChange={handleFileSelect} name="input" types={fileTypes} />
+                <Button
+                    variant="contained"
+                    component="label"
+                    sx={{ marginTop: 1 }}
+                    onClick={handleSubmit}
+                >
+                    Submit
+                </Button>
+                {isLoading ?
+                    <Dialog onClose={toggleDialog} open={dialog} onClick={handleDialogClick} disableBackdropClick>
+                        <DialogContent>
+                            <CircularProgress sx={{ display: "block", left: 0, right: 0, margin: "auto" }} />
+                            Uploading...
+                        </DialogContent>
+                    </Dialog>
+                    :
+                    <>
+                        {resMsg === "Success" ? <Alert sx={{ marginTop: 1 }} severity="success">{resMsg}</Alert> : null}
+                        {resMsg === "No file selected" ? <Alert sx={{ marginTop: 1 }} severity="warning">{resMsg}</Alert> : null}
+                        {resMsg === "Failed" ? <Alert sx={{ marginTop: 1 }} severity="error">{`Failed to upload`}</Alert> : null}
+                    </>
                 }
-            </>
-        )
-    }
+            </FormControl>
+            {previewSrc ?
+                <Card
+                    sx={{ height: "100%", width: "100%", maxWidth: "20rem", display: "block", m: 2 }}
+                    elevation={5}
+                >
+                    <CardMedia
+                        component="img"
+                        width="100%"
+                        image={previewSrc}
+                        alt={"img"}
+                        onClick={() => focusedView(previewSrc)}
+                    />
+                </Card> : null
+            }
+        </>
+    )
 }
+
+export default Upload;
