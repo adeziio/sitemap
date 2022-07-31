fd81680b528799457a51101e0c76738885cd3067
Aden <adeziio@yahoo.com>
2022-07-31 14:38:47 -0400

    revert change back

--- src/api/BackendAPI.js
+++ src/api/BackendAPI.js
@@ -3,7 +3,6 @@ export const fetchGallery = async () => {
         const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/gallery`, {
             "method": "GET",
             "headers": {
-                'Origin': `${process.env.REACT_APP_BACKEND_URL}`,
                 'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
             }
         })
@@ -27,7 +26,6 @@ export const uploadFile = async (file) => {
         const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/upload`, {
             "method": "POST",
             "headers": {
-                'Origin': `${process.env.REACT_APP_BACKEND_URL}`,
                 'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
             },
             "body": formData
@@ -49,7 +47,6 @@ export const extractKey = async (key) => {
         const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/extract?key=${key}`, {
             "method": "GET",
             "headers": {
-                'Origin': `${process.env.REACT_APP_BACKEND_URL}`,
                 'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
             }
         })
@@ -70,7 +67,6 @@ export const deleteKey = async (key) => {
         const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/delete?key=${key}`, {
             "method": "GET",
             "headers": {
-                'Origin': `${process.env.REACT_APP_BACKEND_URL}`,
                 'stargallery-api-key': process.env.REACT_APP_STARGALLERY_API_KEY
             }
         })
